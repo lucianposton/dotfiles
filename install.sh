92f0931a519877eaac3b90996e6e2b2e47a28b8d
Lucian Poston <lucian.poston@gmail.com>
2012-03-05 00:40:45 -0800

    some changes to install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-parse_git_branch() {
-	git branch --no-color 2> /dev/null \
-		| sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) -- /'
+function parse_git_branch {
+   git branch --no-color 2> /dev/null \
+      | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) -- /'
+}
+
+function should_ignore {
+   ignored_files=(
+	"."
+	".."
+	".git"
+	".gitignore"
+	"add.sh"
+	"install.sh"
+	)
+
+	for f in ${ignored_files[@]}
+	do
+		if [[ $f == $1 ]]
+		then
+			return 0
+		fi
+	done
+
+	return 1
 }
 
 mkdir -p ~/dotfiles_old
 
-DotfilesDir=$HOME/dotfiles/
-OldDotfilesDir=$HOME/dotfiles_old/
+DotfilesDir=$HOME/dotfiles
+OldDotfilesDir=$HOME/dotfiles_old
 
 cd $DotfilesDir
-for i in *
+for i in .* *
 do
-	mv ~/.$i $OldDotfilesDir/
-	ln -s $DotfilesDir/$i ~/.$i
+	should_ignore $i && continue
+
+	echo -$i
+	echo "mv ~/$i $OldDotfilesDir/"
+	echo "ln -s $DotfilesDir/$i ~/$i"
+#   mv ~/.$i $OldDotfilesDir/
+#   ln -s $DotfilesDir/$i ~/.$i
 done
 
-rm ~/.make.sh
