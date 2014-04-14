a5fbbd4a16ce6708f41a1472f556322c30f8f2e8
Lucian Poston <lucian.poston@gmail.com>
2014-04-14 10:07:18 -0700

    install.sh updates submodules

--- install.sh
+++ install.sh
@@ -4,24 +4,24 @@ set -e
 
 function should_ignore {
    ignored_files=(
-	"."
-	".."
-	".git"
-	".gitignore"
-	"add.sh"
-	"install.sh"
-	"submodules"
-	)
+       "."
+       ".."
+       ".git"
+       ".gitignore"
+       "add.sh"
+       "install.sh"
+       "submodules"
+   )
 
-	for f in ${ignored_files[@]}
-	do
-		if [[ $f == $1 ]]
-		then
-			return 0
-		fi
-	done
+   for f in ${ignored_files[@]}
+   do
+       if [[ $f == $1 ]]
+       then
+           return 0
+       fi
+   done
 
-	return 1
+   return 1
 }
 
 DotfilesDir=$HOME/dotfiles
@@ -29,8 +29,8 @@ OldDotfilesDir=$HOME/dotfiles_old
 
 if [[ -e $OldDotfilesDir ]]
 then
-	read -n 1 -p "$OldDotfilesDir exists and files will be overwritten. Continue? (y/n)"
-	[[ $REPLY =~ ^[Nn]$ ]] && exit 1
+    read -n 1 -p "$OldDotfilesDir exists and files will be overwritten. Continue? (y/n)"
+    [[ $REPLY =~ ^[Nn]$ ]] && exit 1
 fi
 
 mkdir -p $OldDotfilesDir
@@ -38,8 +38,11 @@ mkdir -p $OldDotfilesDir
 cd $DotfilesDir
 for i in .* *
 do
-	should_ignore $i && continue
-	[[ ! -e $HOME/$i ]] || mv $HOME/$i $OldDotfilesDir/
-	ln -s $DotfilesDir/$i $HOME/$i
+    should_ignore $i && continue
+    [[ ! -e $HOME/$i ]] || mv $HOME/$i $OldDotfilesDir/
+    ln -s $DotfilesDir/$i $HOME/$i
 done
 
+read -n 1 -p "Update submodules? (y/n)"
+[[ $REPLY =~ ^[Nn]$ ]] && exit 0
+cd $DotfilesDir && git submodule update --recursive --init
