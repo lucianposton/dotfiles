6e75e41baa4917f5b31205ae3021d3ca3615ca3a
Lucian Poston <lucian.poston@gmail.com>
2015-11-07 12:14:11 -0800

    add sandbox_user.sh

---
+++ bin/sandbox_user.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+[ $# -ne 4 ] && echo "Usage: $0 category/package sandbox_user_name sandbox_user_home user_name" && exit 1
+
+pkg=$1
+sbuser=$2
+home=$3
+user=$4
+
+useradd --home=$home --create-home --shell /bin/false --user-group $sbuser
+chgrp $user $home
+chmod 770 $home
+
+echo "$user ALL=($sbuser) NOPASSWD: ALL" > /etc/sudoers.d/$sbuser
+
+qlist "$pkg" | xargs chmod u-x,g-w,o-o
+qlist "$pkg" | xargs chown root:$sbuser
+
+bashrc="/etc/portage/env/$pkg"
+
+mkdir -p $(dirname $bashrc)
+
+echo "post_src_install() {
+  chmod -R u-x,g-w,o-o \${D}
+  chown -R root:$sbuser \${D}
+}" > $bashrc
