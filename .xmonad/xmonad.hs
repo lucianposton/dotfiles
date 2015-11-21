95e970ba3a3134e0c157cdb88e027da0fd395ce3
Lucian Poston <lucian.poston@gmail.com>
2015-11-21 13:05:52 -0800

    xmonad ewmh

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,11 +8,13 @@
 --
 
 import XMonad
+import Data.Monoid
 import System.Exit
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 import qualified XMonad.Layout.NoBorders as NB
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Actions.GridSelect
 import Graphics.X11.ExtraTypes.XF86
 
@@ -20,6 +22,14 @@ import Graphics.X11.ExtraTypes.XF86
 -- certain contrib modules.
 --
 myTerminal      = "urxvtcd.sh || urxvtc || urxvt || xterm"
+
+-- Whether focus follows the mouse pointer.
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+-- Whether clicking on a window to focus also passes the click to the window
+myClickJustFocuses :: Bool
+myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
@@ -67,82 +77,86 @@ myFocusedBorderColor = "#A0A0D0"
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
-myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- launch a terminal
-    [ ((modMask .|. myAltMask, xK_Return), spawn $ XMonad.terminal conf)
+    [ ((modm .|. myAltMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch arbitrary script
-    --, ((modMask,               xK_f     ), spawn "~/doit.sh")
+    --, ((modm,               xK_f     ), spawn "~/doit.sh")
 
     -- launch dmenu
-    , ((modMask,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
 
     -- launch gmrun
-    , ((modMask .|. myAltMask, xK_p     ), spawn "gmrun")
+    , ((modm .|. myAltMask, xK_p     ), spawn "gmrun")
 
     -- launch GridSelect
-    , ((modMask,               xK_g     ), goToSelected defaultGSConfig)
+    , ((modm,               xK_g     ), goToSelected defaultGSConfig)
 
     -- launch GridSelect w/ favorites
-    , ((modMask .|. myAltMask, xK_g     ), spawnSelected defaultGSConfig ["firefox", "/home/poston/eclipse/eclipse"])
+    , ((modm .|. myAltMask, xK_g     ), spawnSelected defaultGSConfig ["firefox", "/home/poston/eclipse/eclipse"])
 
-    -- close focused window 
-    , ((modMask .|. myAltMask, xK_c     ), kill)
+    -- close focused window
+    , ((modm .|. myAltMask, xK_c     ), kill)
 
      -- Rotate through the available layout algorithms
-    , ((modMask,               xK_space ), sendMessage NextLayout)
+    , ((modm,               xK_space ), sendMessage NextLayout)
 
     --  Reset the layouts on the current workspace to default
-    , ((modMask .|. myAltMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modm .|. myAltMask, xK_space ), setLayout $ XMonad.layoutHook conf)
 
     -- Resize viewed windows to the correct size
-    , ((modMask,               xK_n     ), refresh)
+    , ((modm,               xK_n     ), refresh)
 
     -- Move focus to the next window
-    , ((modMask,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_Tab   ), windows W.focusDown)
 
     -- Move focus to the next window
-    , ((modMask,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
 
     -- Move focus to the previous window
-    , ((modMask,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_k     ), windows W.focusUp  )
 
     -- Move focus to the master window
-    , ((modMask,               xK_m     ), windows W.focusMaster  )
+    , ((modm,               xK_m     ), windows W.focusMaster  )
 
     -- Swap the focused window and the master window
-    , ((modMask,               xK_Return), windows W.swapMaster)
+    , ((modm,               xK_Return), windows W.swapMaster)
 
     -- Swap the focused window with the next window
-    , ((modMask .|. myAltMask, xK_j     ), windows W.swapDown  )
+    , ((modm .|. myAltMask, xK_j     ), windows W.swapDown  )
 
     -- Swap the focused window with the previous window
-    , ((modMask .|. myAltMask, xK_k     ), windows W.swapUp    )
+    , ((modm .|. myAltMask, xK_k     ), windows W.swapUp    )
 
     -- Shrink the master area
-    , ((modMask,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_h     ), sendMessage Shrink)
 
     -- Expand the master area
-    , ((modMask,               xK_l     ), sendMessage Expand)
+    , ((modm,               xK_l     ), sendMessage Expand)
 
     -- Push window back into tiling
-    , ((modMask,               xK_t     ), withFocused $ windows . W.sink)
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
 
     -- Increment the number of windows in the master area
-    , ((modMask              , xK_comma ), sendMessage (IncMasterN 1))
+    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
 
     -- Deincrement the number of windows in the master area
-    , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
+    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
     -- toggle the status bar gap
-    -- TODO, update this binding with avoidStruts , ((modMask              , xK_b     ),
+    -- Use this binding with avoidStruts from Hooks.ManageDocks.
+    -- See also the statusBar function from Hooks.DynamicLog.
+    --
+    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
     -- Quit xmonad
-    , ((modMask .|. myAltMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm .|. myAltMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
     -- Restart xmonad
-    , ((modMask               .|. shiftMask, xK_q     ), restart "xmonad" True)
+    , ((modm               .|. shiftMask, xK_q     ), restart "xmonad" True)
+
     ]
     ++
 
@@ -150,7 +164,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     --
-    [((m .|. modMask, k), windows $ f i)
+    [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0])
         , (f, m) <- [(W.greedyView, 0), (W.shift, myAltMask)]]
     ++
@@ -159,7 +173,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
-    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_q, xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, myAltMask)]]
     ++
@@ -187,16 +201,16 @@ button15 = 15 :: Button
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
-myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w))
 
     -- mod-button2, Raise the window to the top of the stack
-    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    , ((modm, button2), (\w -> focus w >> windows W.swapMaster))
 
     -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w))
 
     -- launch arbitrary script
     --, ((0,       button10), (\w -> spawn "~/doit.sh"))
@@ -250,10 +264,29 @@ myManageHook = composeAll
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore ]
 
--- Whether focus follows the mouse pointer.
-myFocusFollowsMouse :: Bool
-myFocusFollowsMouse = True
+------------------------------------------------------------------------
+-- Event handling
 
+-- * EwmhDesktops users should change this to ewmhDesktopsEventHook
+--
+-- Defines a custom handler function for X Events. The function should
+-- return (All True) if the default handler is to be run afterwards. To
+-- combine event hooks use mappend or mconcat from Data.Monoid.
+--
+myEventHook = mempty
+
+--myEwmhDesktopsEventHook :: Event -> X All
+--myEwmhDesktopsEventHook e@(ClientMessageEvent
+--    {ev_message_type = mt}) = do
+--    a_aw <- getAtom "_NET_ACTIVE_WINDOW"
+--    curTime <- liftIO getCurrentTime
+--    StartupTime starupTime <- XS.get
+--    if    mt == a_aw
+--       && curTime `diffUTCTime` starupTime <= 5.0
+--       then return (All True)
+--       else ewmhDesktopsEventHook e
+--myEwmhDesktopsEventHook e = ewmhDesktopsEventHook e
+--myEventHook = mempty
 
 ------------------------------------------------------------------------
 -- Status bars and logging
@@ -278,22 +311,31 @@ myLogHook = return ()
 myStartupHook = return ()
 
 ------------------------------------------------------------------------
+-- Setup EWMH
+
+myEwmh :: XConfig a -> XConfig a
+myEwmh c = c { startupHook     = startupHook c     <+> ewmhDesktopsStartup
+             , handleEventHook = handleEventHook c <+> ewmhDesktopsEventHook
+--             , handleEventHook = handleEventHook c <+> fullscreenEventHook <+> ewmhDesktopsEventHook
+             , logHook         = logHook c         <+> ewmhDesktopsLogHook
+             }
+
+------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
 
--- Run xmonad with the settings you specify. No need to modify this.
---
-main = xmonad defaults
+main = xmonad $ myEwmh myDefaultConfig
+
+------------------------------------------------------------------------
+-- Default config
 
 -- A structure containing your configuration settings, overriding
--- fields in the default config. Any you don't override, will 
+-- fields in the default config. Any you don't override, will
 -- use the defaults defined in xmonad/XMonad/Config.hs
--- 
--- No need to modify this.
---
-defaults = defaultConfig {
+myDefaultConfig = defaultConfig {
       -- simple stuff
         terminal           = myTerminal,
         focusFollowsMouse  = myFocusFollowsMouse,
+        clickJustFocuses   = myClickJustFocuses,
         borderWidth        = myBorderWidth,
         modMask            = myModMask,
 --        numlockMask        = myNumlockMask,
@@ -308,6 +350,7 @@ defaults = defaultConfig {
       -- hooks, layouts
         layoutHook         = myLayout,
         manageHook         = myManageHook,
+        handleEventHook    = myEventHook,
         logHook            = myLogHook,
         startupHook        = myStartupHook
     }
