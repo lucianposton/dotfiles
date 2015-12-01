163f0c0673d355bb4977d78a98360288db0b30f0
Lucian Poston <lucian.poston@gmail.com>
2015-12-01 15:06:30 -0800

    notes for brew setup

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -93,5 +93,13 @@ if [[ `uname` == 'Darwin' ]]; then
     #brew install grep --default-names
     #brew install gnu-tar --default-names
     #brew install gawk
+
+    #YOU_MAD_BREWS="boost cmake hunspell libzip libzzip lua51 pcre pkg-config qt5 yajl"
+    #for i in $YOU_MAD_BREWS; do
+    #  brew outdated | grep -q $i && brew upgrade $i
+    #done
+    #for i in $YOU_MAD_BREWS; do
+    #  brew list | grep -q $i || brew install $i
+    #done
 fi
 
