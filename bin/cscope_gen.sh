791cabbb741c2dd0b94e40207f949288306cf657
Lucian Poston <lucian.poston@gmail.com>
2016-03-22 14:26:29 -0700

    add cscope_gen.sh

---
+++ bin/cscope_gen.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+find . -name '*.py' \
+-o -name '*.java' \
+-o -iname '*.[CH]' \
+-o -name '*.cpp' \
+-o -name '*.cc' \
+-o -name '*.hpp'  \
+> cscope.files
+
+# -b: just build
+# -q: create inverted index
+cscope -b -q
