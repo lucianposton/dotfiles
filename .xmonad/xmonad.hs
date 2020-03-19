bb8c5e28a0cae4087243da0c78262a6955af736e
Lucian Poston <lucianposton@pm.me>
2020-03-18 22:05:11 -0500

    .xmonad/xmonad.hs: add color-pick key

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -103,6 +103,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     --, ((modm,               xK_f     ), spawn "~/doit.sh")
     , ((modm,               xK_o     ), spawn "ss=/tmp/img-$(date --utc +%FT%T.%3NZ).png; maim -B -i $(xdotool getactivewindow) $ss; imgurbash2 $ss")
     , ((modm .|. myAltMask, xK_o     ), spawn "ss=/tmp/img-$(date --utc +%FT%T.%3NZ).png; maim -B -s $ss; imgurbash2 $ss")
+    , ((modm .|. shiftMask, xK_o     ), spawn "color-picker")
 
     -- launch dmenu
     , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
